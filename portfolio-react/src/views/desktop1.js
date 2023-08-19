b183b5e7d25c78cbb3dd9b5429438b6be9529dac
Gaurav Jain <116191299+G-the-dev@users.noreply.github.com>
2023-08-20 03:58:08 +0530

    Add files via upload

---
+++ portfolio-react/src/views/desktop1.js
@@ -0,0 +1,500 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './desktop1.css'
+
+const Desktop1 = (props) => {
+  return (
+    <div className="desktop1-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="desktop1-desktop1">
+        <img
+          src="/external/ellipse44511-hw3.svg"
+          alt="Ellipse44511"
+          className="desktop1-ellipse4"
+        />
+        <img
+          src="/external/line113875-cli8.svg"
+          alt="Line113875"
+          className="desktop1-line11"
+        />
+        <img
+          src="/external/line113875-cli8.svg"
+          alt="Line113875"
+          className="desktop1-line111"
+        />
+        <div className="desktop1-form">
+          <span className="desktop1-text">
+            <span>Get in touch</span>
+          </span>
+          <span className="desktop1-text02">
+            <span>
+              <span>
+                I&apos;m a Product Designer and Visual Developer currently
+                seeking Full Time Remote Job Opportunities. I specialize in
+                UI/UX Design, Responsive Web Design and Visual Development.
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <span></span>
+              <br></br>
+              <span>
+                You can send me an e-mail directly, connect with me on Social
+                Media or complete the
+              </span>
+              <br></br>
+              <span>
+                Contact Form. I&apos;ll get back to you as soon as possible.
+              </span>
+            </span>
+          </span>
+          <span className="desktop1-text11">
+            <span>Your Name</span>
+          </span>
+          <div className="desktop1-group4">
+            <span className="desktop1-text13">*</span>
+            <span className="desktop1-text14">*</span>
+            <span className="desktop1-text15">*</span>
+          </div>
+          <span className="desktop1-text16">
+            <span>E-mail Address</span>
+          </span>
+          <span className="desktop1-text18">
+            <span>How did you know about me?</span>
+          </span>
+          <span className="desktop1-text20">
+            <span>Your Message</span>
+          </span>
+          <div className="desktop1-group5">
+            <img
+              src="/external/rectangle44510-fkp-200h.png"
+              alt="Rectangle44510"
+              className="desktop1-rectangle4"
+            />
+            <img
+              src="/external/rectangle54510-k7u4-200h.png"
+              alt="Rectangle54510"
+              className="desktop1-rectangle5"
+            />
+            <img
+              src="/external/rectangle64510-zko3-200h.png"
+              alt="Rectangle64510"
+              className="desktop1-rectangle6"
+            />
+            <img
+              src="/external/rectangle74510-2fn-300h.png"
+              alt="Rectangle74510"
+              className="desktop1-rectangle7"
+            />
+          </div>
+          <img
+            src="/external/line124510-589d.svg"
+            alt="Line124510"
+            className="desktop1-line12"
+          />
+          <div className="desktop1-btn2in">
+            <span className="desktop1-text22">
+              <span>SEND MESSAGE!</span>
+            </span>
+          </div>
+          <span className="desktop1-text24">
+            <span>Fill out the Form</span>
+          </span>
+          <img
+            src="/external/java5743-6yvd-200h.png"
+            alt="Java5743"
+            className="desktop1-java"
+          />
+          <img
+            src="/external/androidos5747-aimr-200h.png"
+            alt="AndroidOS5747"
+            className="desktop1-android-os"
+          />
+          <div className="desktop1-group9">
+            <img
+              src="/external/graphql5745-78dc-200h.png"
+              alt="Graphql5745"
+              className="desktop1-graphql"
+            />
+          </div>
+        </div>
+        <img
+          src="/external/sql5755-g2k-200h.png"
+          alt="SQL5755"
+          className="desktop1-sql"
+        />
+        <img
+          src="/external/ellipse54511-9lmn.svg"
+          alt="Ellipse54511"
+          className="desktop1-ellipse5"
+        />
+        <img
+          src="/external/line92696-kvdo.svg"
+          alt="Line92696"
+          className="desktop1-line9"
+        />
+        <div className="desktop1-group7">
+          <img
+            src="/external/image12479-spjh-600h.png"
+            alt="image12479"
+            className="desktop1-image1"
+          />
+          <img
+            src="/external/image22480-v39e-600h.png"
+            alt="image22480"
+            className="desktop1-image2"
+          />
+          <div className="desktop1-group2">
+            <div className="desktop1-frame2">
+              <span className="desktop1-text26">
+                <span>All</span>
+              </span>
+              <span className="desktop1-text28">
+                <span>UI/UX Designs</span>
+              </span>
+              <span className="desktop1-text30">
+                <span>Website Design</span>
+              </span>
+              <span className="desktop1-text32">
+                <span>Web Development</span>
+              </span>
+            </div>
+            <img
+              src="/external/line52487-bm0m.svg"
+              alt="Line52487"
+              className="desktop1-line5"
+            />
+            <img
+              src="/external/line62490-yz5t.svg"
+              alt="Line62490"
+              className="desktop1-line6"
+            />
+            <img
+              src="/external/line72491-hcbg.svg"
+              alt="Line72491"
+              className="desktop1-line7"
+            />
+            <img
+              src="/external/line82492-ft9p.svg"
+              alt="Line82492"
+              className="desktop1-line8"
+            />
+          </div>
+          <span className="desktop1-text34">
+            <span>
+              I design &amp; create unique tailor-made websites and user
+              interfaces! I have worked in startups and as well as
+              well-established companies. You can learn about how I turn ideas
+              into real life products. Please find below projects that I can
+              share with you.
+            </span>
+          </span>
+          <div className="desktop1-group3">
+            <span className="desktop1-text36">
+              <span>Portfolio</span>
+            </span>
+            <img
+              src="/external/line32446-d4ic.svg"
+              alt="Line32446"
+              className="desktop1-line3"
+            />
+            <img
+              src="/external/line54077-7ky.svg"
+              alt="Line54077"
+              className="desktop1-line51"
+            />
+            <img
+              src="/external/line64078-stsl.svg"
+              alt="Line64078"
+              className="desktop1-line61"
+            />
+            <img
+              src="/external/line42450-2zoc.svg"
+              alt="Line42450"
+              className="desktop1-line4"
+            />
+          </div>
+        </div>
+        <div className="desktop1-frame1">
+          <div className="desktop1-object-other11">
+            <img
+              src="/external/objectother07i202-90mp-300w.png"
+              alt="ObjectOther07I202"
+              className="desktop1-object-other07"
+            />
+          </div>
+          <div className="desktop1-object-other071">
+            <img
+              src="/external/objectother072022-psvl7-300w.png"
+              alt="ObjectOther072022"
+              className="desktop1-object-other072"
+            />
+          </div>
+          <div className="desktop1-object-other9">
+            <img
+              src="/external/objectother07i202-luz-300w.png"
+              alt="ObjectOther07I202"
+              className="desktop1-object-other073"
+            />
+          </div>
+          <div className="desktop1-object-other8">
+            <img
+              src="/external/objectother07i202-xs-300w.png"
+              alt="ObjectOther07I202"
+              className="desktop1-object-other074"
+            />
+          </div>
+          <div className="desktop1-object-other10">
+            <img
+              src="/external/objectother07i202-13ha-300w.png"
+              alt="ObjectOther07I202"
+              className="desktop1-object-other075"
+            />
+          </div>
+        </div>
+        <div className="desktop1-group1">
+          <span className="desktop1-text38">
+            <span>Notable Projects I’ve worked on</span>
+          </span>
+          <img
+            src="/external/line12234-7yb6.svg"
+            alt="Line12234"
+            className="desktop1-line1"
+          />
+          <img
+            src="/external/line22235-wac.svg"
+            alt="Line22235"
+            className="desktop1-line2"
+          />
+        </div>
+        <img
+          src="/external/line102898-mmwe.svg"
+          alt="Line102898"
+          className="desktop1-line10"
+        />
+        <img
+          src="/external/ellipse2148-ho2.svg"
+          alt="Ellipse2148"
+          className="desktop1-ellipse2"
+        />
+        <div className="desktop1-coder">
+          <img
+            src="/external/image2334-0q48-500h.png"
+            alt="image2334"
+            className="desktop1-image21"
+          />
+          <span className="desktop1-text40">
+            <span>
+              <span>
+                Front developer who writes
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <span>clean.,elegant and efficient code</span>
+            </span>
+          </span>
+          <span className="desktop1-text45">
+            <span>&lt;coder&gt;</span>
+          </span>
+          <div className="desktop1-btn2in1">
+            <span className="desktop1-text47">
+              <span>BUTTON</span>
+            </span>
+          </div>
+        </div>
+        <img
+          src="/external/ellipse64511-c7lk.svg"
+          alt="Ellipse64511"
+          className="desktop1-ellipse6"
+        />
+        <img
+          src="/external/ellipse3149-liy.svg"
+          alt="Ellipse3149"
+          className="desktop1-ellipse3"
+        />
+        <div className="desktop1-designer">
+          <img
+            src="/external/image1315-95g-500h.png"
+            alt="image1315"
+            className="desktop1-image11"
+          />
+          <span className="desktop1-text49">
+            <span>
+              <span>UI/UX product designer specializing</span>
+              <br></br>
+              <span> in UI designs and design systems.</span>
+            </span>
+          </span>
+          <span className="desktop1-text54">
+            <span>/designer/</span>
+          </span>
+          <div className="desktop1-btn1in">
+            <span className="desktop1-text56">
+              <span>BUTTON</span>
+            </span>
+          </div>
+        </div>
+        <div className="desktop1-header">
+          <span className="desktop1-text58">
+            <span>Portfolio.</span>
+          </span>
+          <span className="desktop1-text60">
+            <span>Home</span>
+          </span>
+          <span className="desktop1-text62">
+            <span>About</span>
+          </span>
+          <span className="desktop1-text64">
+            <span>Blog</span>
+          </span>
+          <span className="desktop1-text66">
+            <span>Projects</span>
+          </span>
+          <span className="desktop1-text68">
+            <span>Contact</span>
+          </span>
+        </div>
+        <img
+          src="/external/line5538-95aj.svg"
+          alt="Line5538"
+          className="desktop1-line52"
+        />
+        <img
+          src="/external/line12539-javi.svg"
+          alt="Line12539"
+          className="desktop1-line121"
+        />
+        <div className="desktop1-group6">
+          <img
+            src="/external/ellipse75318-s1qv-200h.png"
+            alt="Ellipse75318"
+            className="desktop1-ellipse7"
+          />
+          <img
+            src="/external/rectangle85319-2jqb-200h.png"
+            alt="Rectangle85319"
+            className="desktop1-rectangle8"
+          />
+          <img
+            src="/external/line145311-sv1l.svg"
+            alt="Line145311"
+            className="desktop1-line14"
+          />
+        </div>
+        <img
+          src="/external/linearessentionaluicopyright5325-oi5v.svg"
+          alt="LinearEssentionalUICopyright5325"
+          className="desktop1-linear-essentional-u-copyright"
+        />
+        <span className="desktop1-text70">
+          <span>Portfolio</span>
+        </span>
+        <div className="desktop1-group8">
+          <img
+            src="/external/googleplus5729-mahe-200h.png"
+            alt="GooglePlus5729"
+            className="desktop1-google-plus"
+          />
+          <img
+            src="/external/facebook5730-twrc-200h.png"
+            alt="Facebook5730"
+            className="desktop1-facebook"
+          />
+          <img
+            src="/external/instagram5731-8p6lb-200h.png"
+            alt="Instagram5731"
+            className="desktop1-instagram"
+          />
+          <img
+            src="/external/linkedin5732-4av-200h.png"
+            alt="LinkedIn5732"
+            className="desktop1-linked-in"
+          />
+          <img
+            src="/external/pinterest5733-2vpk-200h.png"
+            alt="Pinterest5733"
+            className="desktop1-pinterest"
+          />
+          <img
+            src="/external/twitterx5734-n2ob-200h.png"
+            alt="TwitterX5734"
+            className="desktop1-twitter-x"
+          />
+          <img
+            src="/external/discord5735-1ggi-200h.png"
+            alt="Discord5735"
+            className="desktop1-discord"
+          />
+          <img
+            src="/external/whatsapp5736-omp5-200h.png"
+            alt="WhatsApp5736"
+            className="desktop1-whats-app"
+          />
+          <img
+            src="/external/snapchat5737-zheh-200h.png"
+            alt="Snapchat5737"
+            className="desktop1-snapchat"
+          />
+          <img
+            src="/external/telegramapp5738-25q-200h.png"
+            alt="TelegramApp5738"
+            className="desktop1-telegram-app"
+          />
+          <img
+            src="/external/google5739-twazs-200h.png"
+            alt="Google5739"
+            className="desktop1-google"
+          />
+          <img
+            src="/external/github5740-ngun-200h.png"
+            alt="GitHub5740"
+            className="desktop1-git-hub"
+          />
+          <img
+            src="/external/wordpress5748-cp4-200h.png"
+            alt="WordPress5748"
+            className="desktop1-word-press"
+          />
+        </div>
+        <img
+          src="/external/python5741-zawt-200h.png"
+          alt="Python5741"
+          className="desktop1-python"
+        />
+        <img
+          src="/external/javascript5742-mak-200h.png"
+          alt="JavaScript5742"
+          className="desktop1-java-script"
+        />
+        <img
+          src="/external/html55744-q8ur8-200h.png"
+          alt="Html55744"
+          className="desktop1-html5"
+        />
+        <img
+          src="/external/css35746-ljf-200h.png"
+          alt="CSS35746"
+          className="desktop1-css3"
+        />
+        <img
+          src="/external/figma5754-jx3k-200h.png"
+          alt="Figma5754"
+          className="desktop1-figma"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Desktop1
